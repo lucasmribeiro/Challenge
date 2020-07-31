0abe5559fc3e62064ee258cb664b5627178fc4b1
Lucas Ribeiro <lucas.ribeiro.007@acad.pucrs.br>
2020-07-31 13:03:27 -0300

    Implemented the replace for special characters

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Challenge
 Desafio de programação para seleção no LIS
+
+# References
+
+[Wikipedia](https://pt.wikipedia.org/wiki/Cifra_de_C%C3%A9sar)
--- main.cpp
+++ main.cpp
@@ -2,61 +2,60 @@
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include <regex>
 
 using namespace std;
 
 /* Constants */
 const string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,;!?";
 
-/* Variables */
-ifstream fileIN;
-ofstream fileOUT;
-
 /* Prototypes */
 int readFile(string filename, string &message);
 int writeFile(string filename, string &message);
 void encryptMessage(int key, string &message);
 void decryptMessage(int key, string &message);
+void removeCharacteres(string &message);
 
 /* Functions */
 int readFile(string filename, string &message)
 {
+    ifstream file;
     string line;
-    fileIN.open(filename, ios::in);
-    if(!fileIN)
+    file.open(filename, ios::in);
+    if(!file)
     {
         cout << "Failed to read file." << endl;
         return 1;
     }
-    while(!fileIN.eof())
+    while(!file.eof())
     {
-        getline(fileIN, line);
+        getline(file, line);
         message.append(line);
         message.append("\n");
     }
-    fileIN.close();
+    file.close();
     return 0;
 }
-
 int writeFile(string filename, string &message)
 {
-    fileOUT.open(filename, ios::trunc);
-    if(!fileOUT)
+    ofstream file;
+    file.open(filename, ios::trunc);
+    if(!file)
     {
         cout << "Failed to write file." << endl;
         return 1;
     }
-    fileOUT << message << endl;
-    fileOUT.close();
+    file << message << endl;
+    file.close();
     return 0;
 }
-
 void encryptMessage(int key, string &message)
 {
+    // Encrypts character by character
     for(size_t k = 0; k < message.size(); k++)
     {
-        size_t pos = letters.find(message[k], 0);
-        if(k != -1)
+        int pos = letters.find(message[k], 0);
+        if(pos != -1)
         {
             message[k] = letters[(pos + key) % letters.size()];
         }
@@ -66,13 +65,13 @@ void encryptMessage(int key, string &message)
         }
     }
 }
-
 void decryptMessage(int key, string &message)
 {
+    // Decrypts character by character
     for(size_t k = 0; k < message.size(); k++)
     {
-        size_t pos = letters.find(message[k], 0);
-        if(k != -1)
+        int pos = letters.find(message[k], 0);
+        if(pos != -1)
         {
             message[k] = letters[(pos + letters.size() - key) % letters.size()];
         }
@@ -82,18 +81,55 @@ void decryptMessage(int key, string &message)
         }
     }
 }
+void removeCharacteres(string &message)
+{
+    regex re ("[.,;!?]");
+    string aux;
+    cout << regex_replace(message, re, "") << endl;
 
+    cout << aux << endl;
+}
 int main(int argc, char* argv[])
 {
-    string filename, message;
+    // Array of string messages decrypt
+    string messages[letters.size()];
+    //
+    string filename;
+    //
+    string message;
+    //
+    string aux;
+
+    // Get the filename of message
     filename = argv[1];
+    // Read file with the message
     if(readFile(filename, message))
     {
         return -1;
     }
 
+    // Decrypt the message with n-key [0 ... 30]
+    for(size_t k = 0; k < letters.size(); k++)
+    {
+        // Copy the message to aux
+        aux.assign(message);
+        decryptMessage(k, aux);
+        // Store the message descrypted in array of messages
+        messages[k] = aux;
+    }
+
+    aux = messages[19];
+    removeCharacteres(aux);
+
+    for(size_t k = 0; k < letters.size(); k++)
+    {
+        cout << messages[k] << endl;
+    }
+
+    // Get filename of the destination
     filename = argv[2];
-    if(writeFile(filename, message))
+    // Write file with a correct message decrypted
+    if(writeFile(filename, messages[19]))
     {
         return -1;
     }
